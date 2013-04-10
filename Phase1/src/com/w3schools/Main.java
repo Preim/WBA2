f23e0130dcdd374ea3197ac6d8d228fe2b968dba
Parick Reimringer <p.reimringer@hotmail.de>
2013-04-10 15:36:15 +0200

    aufgabe3 final

--- Phase1/src/com/w3schools/Main.java
+++ Phase1/src/com/w3schools/Main.java
@@ -6,7 +6,8 @@ import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-import com.w3schools.Chefkoch.Rezept;
+import com.w3schools.Chefkoch.Rezept.Zubereitung;
+import com.w3schools.Chefkoch.Rezept.Zutatenliste.Zutat;
 
 public class Main {
 	
@@ -15,19 +16,26 @@ public class Main {
 	
 	public static void main(String [] args)throws Exception{
 		
-		Chefkoch cf = new Chefkoch();
 		JAXBContext context = JAXBContext.newInstance(Chefkoch.class);
-//		Marshaller m = context.createMarshaller();
-//		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-//		m.marshal(cf, System.out);
-		
-		System.out.println("Output from our XML File: ");
-	    Unmarshaller um = context.createUnmarshaller();
-	    Chefkoch bookstore2 = (Chefkoch) um.unmarshal(new FileReader(xml_file));
-	    List<Rezept> list = bookstore2.rezept;
-	    for (Chefkoch.Rezept rezept : list) {
-	      System.out.println(rezept.info.getUeberschrift());
+	    Unmarshaller um = context.createUnmarshaller();	    
+	    Chefkoch chefkoch = (Chefkoch) um.unmarshal(new FileReader(xml_file));
+	    
+	    for(int i = 0; i<chefkoch.getRezept().size();i++){
+	    	System.out.println("\nRezeptnummer: "+chefkoch.getRezept().get(i).id);
+	    	System.out.println(chefkoch.getRezept().get(i).info.getUeberschrift());
+	    	System.out.println(chefkoch.getRezept().get(i).info.getBild());
+	    	System.out.println("\n----Zutaten----\n");
+	    	for(int j = 0;j<chefkoch.getRezept().get(i).getZutatenliste().getZutat().size();j++){
+	    		List<Zutat> zutaten = chefkoch.getRezept().get(i).getZutatenliste().getZutat();
+	    		System.out.println(zutaten.get(j).getBezeichnung()+": "+zutaten.get(j).getMenge()+" "+zutaten.get(j).getEinheit());
+	    	}
+	    	System.out.println("\n----Zubereitung----\n");
+	    	Zubereitung zb = chefkoch.getRezept().get(i).getZubereitung();
+	    	System.out.println("Zubereitung: "+zb.getAnleitung());
+	    	System.out.println("Schwierigkeitsgrad: "+ zb.getSchwierigkeitsgrad());
+	    	System.out.println("Brennwert: "+zb.getBrennwert());
+	    	System.out.println("Arbeitszeit: "+zb.getArbeitszeit());
+	    	
 	    }
-
-	}
+  }
 }
