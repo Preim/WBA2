33158ae1da4e7057bb2baf41f720853afc2782b2
Parick Reimringer <p.reimringer@hotmail.de>
2013-04-13 03:39:40 +0200

    update fix

--- Phase1/src/com/w3schools/Main.java
+++ Phase1/src/com/w3schools/Main.java
@@ -1,9 +1,12 @@
 package com.w3schools;
 
+import java.io.File;
 import java.io.FileReader;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import com.w3schools.Chefkoch.Rezept.Zubereitung;
@@ -12,13 +15,38 @@ import com.w3schools.Chefkoch.Rezept.Zutatenliste.Zutat;
 public class Main {
 	
 	private static final String xml_file = "/Users/Patrick/Git/WBA2/git/Phase1/src/Aufgabe3.xml";
-	
+	private static JAXBContext context;
+	private static Chefkoch chefkoch;
+	private static Unmarshaller um;
+	private static Marshaller m;
 	
 	public static void main(String [] args)throws Exception{
 		
-		JAXBContext context = JAXBContext.newInstance(Chefkoch.class);
-	    Unmarshaller um = context.createUnmarshaller();	    
-	    Chefkoch chefkoch = (Chefkoch) um.unmarshal(new FileReader(xml_file));
+		context = JAXBContext.newInstance(Chefkoch.class);
+		um = context.createUnmarshaller();
+		m = context.createMarshaller();
+		
+		chefkoch = (Chefkoch) um.unmarshal(new FileReader(xml_file));
+		Scanner in = new Scanner(System.in);
+		Scanner in_string = new Scanner(System.in);
+		int eingabe = 1;
+		
+		while (eingabe != 0){
+			System.out.println("\nWas wollen Sie machen?:\n 1 für eintragen\n 2 für ausgeben\n 0 für exit");
+			eingabe = in.nextInt();
+			if(eingabe == 1){
+				System.out.println("In welches Rezept soll die Nachricht eingetragen werden?\n RezeptID: ");
+					int rezept = in.nextInt();
+						System.out.println("Kommentar eingeben:");
+							String nachricht = in_string.nextLine();
+								eingabe(rezept-1,nachricht);
+			}
+			if(eingabe == 2) ausgabe();
+		}
+	    	    
+  }
+	
+	private static void ausgabe()throws Exception{	    	    
 	    
 	    for(int i = 0; i<chefkoch.getRezept().size();i++){
 	    	System.out.println("\nRezeptnummer: "+chefkoch.getRezept().get(i).id);
@@ -35,7 +63,24 @@ public class Main {
 	    	System.out.println("Schwierigkeitsgrad: "+ zb.getSchwierigkeitsgrad());
 	    	System.out.println("Brennwert: "+zb.getBrennwert());
 	    	System.out.println("Arbeitszeit: "+zb.getArbeitszeit());
+	    	System.out.println("\n Kommentare:");
+	    	for(int k=0;k<chefkoch.getRezept().get(i).getKommentare().getNachricht().size();k++){
+	    		System.out.println(chefkoch.getRezept().get(0).getKommentare().getNachricht().get(k));
+	    	}
 	    	
 	    }
-  }
+		
+	}
+	
+	private static void eingabe(int rezeptnr,String nachricht)throws Exception{
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//		if(chefkoch.getRezept().get(rezeptnr).getKommentare().getNachricht().get(0).equals("")){
+//			chefkoch.getRezept().get(rezeptnr).getKommentare().getNachricht().add(0,nachricht);
+//		}else{
+		chefkoch.getRezept().get(rezeptnr).getKommentare().getNachricht().add(nachricht);
+//		}
+
+		m.marshal(chefkoch, new File(xml_file));
+		
+	}
 }
